368665925b326ee94364de4a6b7bcec9be7ac532
stoolan <stoolan@telegeography.com>
2020-01-29 12:02:06 -0500

    updated railtie

--- lib/bootstrap_helpers/railtie.rb
+++ lib/bootstrap_helpers/railtie.rb
@@ -2,7 +2,7 @@ require "bootstrap_helpers/helper"
 module BootstrapHelpers
   class Railtie < ::Rails::Railtie
     initializer "bootstrap_helpers.helper" do |_app|
-      ActionView::Base.send :include, BootstrapHelpers::Helper
+      ActionView::Base.send :include, BootstrapHelpers
     end
   end
 end
